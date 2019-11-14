91cb025fe0042e406f876afecf4de71e60f0f1ef
岡島諒介 <>
2019-11-15 01:42:21 +0900

    no message

---
+++ Assets/ItemGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour {
+
+	public GameObject Ring;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 10; i++){
+			float RingPosX = Random.Range(4, 497);
+			float RingPosZ = Random.Range (4, 497);
+			GameObject ring = Instantiate (Ring) as GameObject;
+			ring.transform.position = new Vector3 (RingPosX, 150, RingPosZ);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Point.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Point : MonoBehaviour {
+
+	private GameObject MessageText;
+	private GameObject PushKeyText;
+	private GameObject RingPointText;
+	private int RingPoint = 0;
+
+	// Use this for initialization
+	void Start () {
+		this.MessageText = GameObject.Find ("MessageText");
+		this.PushKeyText = GameObject.Find ("PushKeyText");
+		this.RingPointText = GameObject.Find ("RingPointText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (RingPoint == 10) {
+			this.MessageText.GetComponent<Text> ().text = "Clear!!";
+			this.PushKeyText.GetComponent<Text> ().text =
+				"PushKey [R] : Restart\nPushKey [E] : Return to Menu";
+			if (Input.GetKey(KeyCode.R)) {
+				SceneManager.LoadScene ("GameScene");
+			}
+		}
+	}
+
+	void OnCollisionEnter (Collision Other){
+		if (Other.gameObject.tag == "RingTag") {
+			RingPoint += 1;
+			string RingP = RingPoint.ToString ();
+			this.RingPointText.GetComponent<Text> ().text = "Ring : " + RingP;
+			Destroy (Other.gameObject);
+		}
+	}
+}
